3e2a9509f6acd2bbef3e3e5bb0040cefe3f3778e
A107070014 <fesu308@gmail.com>
2023-03-03 00:29:50 +0800

    [modify]pointer text

--- components/Layout/Footer/index.js
+++ components/Layout/Footer/index.js
@@ -23,7 +23,7 @@ export default function Footer() {
     <FooterContainer>
       <FooterOptionContainer>
         <LeftFooterContainer>
-          <JoinUs cursor>Join Us On</JoinUs>
+          <JoinUs>Join Us On</JoinUs>
           <IconContainer>
             <FacebookIcon />
             <IgIcon />
@@ -34,39 +34,39 @@ export default function Footer() {
         <Divider></Divider>
         <RightFooterContainer>
           <FooterLine>
-            <Text white cursor Size="18px">
+            <Text white Size="18px">
               公司資訊
             </Text>
-            <Text white>關於我們</Text>
-            <Text white>媒體報導</Text>
-            <Text white>聯繫方式</Text>
+            <Text white cursor="pointer">關於我們</Text>
+            <Text white cursor="pointer">媒體報導</Text>
+            <Text white cursor="pointer">聯繫方式</Text>
           </FooterLine>
           <FooterLine>
-            <Text white cursor Size="18px">
+            <Text white Size="18px">
               網站條款
             </Text>
-            <Text white>操作協助</Text>
-            <Text white>服務條款</Text>
-            <Text white>安全條款</Text>
-            <Text white>隱私說明</Text>
-            <Text white>賠償保證</Text>
-            <Text white>付款說明</Text>
+            <Text white cursor="pointer">操作協助</Text>
+            <Text white cursor="pointer">服務條款</Text>
+            <Text white cursor="pointer">安全條款</Text>
+            <Text white cursor="pointer">隱私說明</Text>
+            <Text white cursor="pointer">賠償保證</Text>
+            <Text white cursor="pointer">付款說明</Text>
           </FooterLine>
           <FooterLine>
-            <Text white cursor Size="18px">
+            <Text white Size="18px">
               時間共享
             </Text>
-            <Text white>時間共享</Text>
-            <Text white>在地達人</Text>
-            <Text white>旅行交友</Text>
+            <Text white cursor="pointer">時間共享</Text>
+            <Text white cursor="pointer">在地達人</Text>
+            <Text white cursor="pointer">旅行交友</Text>
           </FooterLine>
         </RightFooterContainer>
       </FooterOptionContainer>
       <CopyRightContatiner>
-        <Text white cursor Size="18px">
+        <Text white Size="18px">
           本平台僅提供媒合功能，各在地達人之資格與服務內容，須符合當地政府規定。
         </Text>
-        <Text white cursor>© 2020 tour guide All rights reserved.</Text>
+        <Text white>© 2020 tour guide All rights reserved.</Text>
       </CopyRightContatiner>
     </FooterContainer>
   );
